cfcbc21ed8097c401fc2a6fabd80abe6dc40aa13
junkai.wang <openwjk@gmail.com>
2023-07-30 08:45:36 +0800

    提交 ResponseEnum

---
+++ src/main/java/com/openwjk/commons/enums/ResponseEnum.java
@@ -0,0 +1,40 @@
+package com.openwjk.commons.enums;
+
+/**
+ * @author wangjunkai
+ * @description
+ * @date 2023/7/30 8:33
+ */
+public enum ResponseEnum {
+    SUCCESS("00", "success."),
+
+    UNAUTHORIZED("10", "unauthorized."),
+    SIGN_CHECK_FAIL("11", "signature is invalid."),
+    PARAM_FORMAT_ERROR("12", "check param error."),
+    INSUFFICIENT_PERMISSION("13", "insufficient permissions."),
+    IP_NOT_ALLOW_ACCESS("14", "ip don't allow access."),
+
+    PARAM_CHECK_FAIL("20", "param is invalid."),
+    PARAM_STRATEGY_INVALID("21", "request strategy is invalid."),
+    RPC_FAIL("30", "call remote service occur error."),
+
+    NOT_ALLOWED("40", "current status don't allow operation."),
+
+    SYSTEM_ERROR("99", "error.");
+
+    private String code;
+    private String msg;
+
+    ResponseEnum(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
